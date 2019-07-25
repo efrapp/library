51a52ffabab93b29a49e2673e6cc48bb74d1e954
Efrain Pinto <efrapp@gmail.com>
2019-07-25 08:06:01 -0500

    Update syntax with linter suggestions

--- js/application.js
+++ js/application.js
@@ -1,31 +1,31 @@
-let myLibrary = [];
+'use strict'
 
-function addBookToLibrary(author, title, pages, read){
-  let book = new Book(author, title, pages, read);
-  myLibrary.push(book);
+const myLibrary = []
+
+function addBookToLibrary (author, title, pages, read) {
+  const book = new Book(author, title, pages, read)
+  myLibrary.push(book)
 }
 
-(function(){
-
+(() => {
   // Create 2 sample books
-  let sampleBooks = [
+  const sampleBooks = [
     {
-      author: "Ricardo Peña Marí",
+      author: 'Ricardo Peña Marí',
       title: "Design's Programms",
       pages: 466,
       read: false
     },
     {
-      author: "Norton Juster",
-      title: "The Phantom Tollbooth",
+      author: 'Norton Juster',
+      title: 'The Phantom Tollbooth',
       pages: 255,
       read: true
-    },
-  ];
+    }
+  ]
 
   sampleBooks.forEach(b => {
-    addBookToLibrary(b.author, b.title, b.pages, b.read);
-  });
-    
-  console.log(myLibrary);
-})();
+    addBookToLibrary(b.author, b.title, b.pages, b.read)
+  })
+  console.log(myLibrary)
+})()
