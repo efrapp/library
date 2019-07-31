2540eb4ce29ab87451234e0403b41f1bdfb22d9e
Efrain Pinto <efrapp@gmail.com>
2019-07-31 08:25:32 -0500

    Create form to add new books to Bookstore

--- js/application.js
+++ js/application.js
@@ -1,9 +1,34 @@
 const myLibrary = [];
 
-function addBookToLibrary(author, title, pages, read) {
-  const book = new Book(author, title, pages, read);
+function addBookToLibrary(author, title, pages, isbn, read) {
+  const book = new Book(author, title, pages, isbn, read);
   myLibrary.push(book);
+  console.log(myLibrary);
 }
+
+function createNewBook() {
+  const form = document.getElementById('newBook');
+  if (form.elements.author.value === '') {
+    M.toast({ html: 'Please fill the Author information' });
+  } else if (form.elements.title.value === '') {
+    M.toast({ html: 'Please fill the Title information' });
+  } else if (form.elements.pages.value === '') {
+    M.toast({ html: 'Please fill the Pages information' });
+  } else {
+    addBookToLibrary(
+      form.elements.author.value,
+      form.elements.title.value,
+      parseInt(form.elements.pages.value, 10),
+      parseInt(form.elements.isbn.value, 10),
+      form.elements.read.checked,
+    );
+    form.reset();
+    M.updateTextFields();
+    M.toast({ html: 'Book created!' });
+  }
+}
+
+document.getElementById('save').onclick = createNewBook;
 
 (() => {
   // Create 2 sample books
@@ -12,17 +37,19 @@ function addBookToLibrary(author, title, pages, read) {
       author: 'Ricardo Peña Marí',
       title: "Design's Programms",
       pages: 466,
+      isbn: 1,
       read: false,
     },
     {
       author: 'Norton Juster',
       title: 'The Phantom Tollbooth',
       pages: 255,
+      isbn: 2,
       read: true,
     },
   ];
 
   sampleBooks.forEach((b) => {
-    addBookToLibrary(b.author, b.title, b.pages, b.read);
+    addBookToLibrary(b.author, b.title, b.pages, b.isbn, b.read);
   });
 })();
--- js/book.js
+++ js/book.js
@@ -1,8 +1,9 @@
 class Book {
-  constructor(author, title, pages, read) {
+  constructor(author, title, pages, isbn = 0, read = false) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.isbn = isbn;
     this.read = read;
   }
 }
